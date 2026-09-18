baf68d503f1c5aafaf006bf974b748c30f6d4d25
angel3122004 <angelmary23cs@cce.edu.in>
2026-09-18 09:34:53 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,1 +1,12 @@
+import streamlit as st
+import joblib
+from sklearn.preprocessing import PolynomialFeatures
 
+model=joblib.load("Polynomial_Regression_ElectricBill_Model.pkl")
+st.title("Electricity Bill Prediction Based On AC Units")
+ac_units=st.number_input("Enter AC Units : ", min_value=0.0, value=100.0)
+if st.button("Predict"):
+  poly=PolynomialFeatures()
+  ac_units_poly=poly.fit_transform([[ac_units]])
+  prediction=model.predict(ac_units_poly)
+  st.success(f"Predicted Electricity Bill : {prediction[0]:.2f}")
